2a0641f5f3e492f1f992cdc9754df67dd9c03e7a
chooshihao <chooshihao@gmail.com>
2022-09-09 21:18:34 +0800

    refractor the app app.py - refractor to factory pattern to better facillitate testing - rename ShortURL.py to models - added blueprint to better organise endpoints

--- app/app.py
+++ app/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, request, redirect, jsonify
-from markupsafe import escape
-from models.ShortURL import ShortURL, db
-from hashids import Hashids
 import configparser
 import os
+from flask import Flask
+from models import db
+from shortener_bp import shortener_bp
 
-def main():
+def loadConfig():
 	config = configparser.ConfigParser()
 	config.read(os.path.dirname(os.path.realpath(__file__)) + "/app.conf")
-	config.sections()
+	print(config.sections())
+
+	return config
+
+def init_app():
+	config = loadConfig()
 
 	app = Flask(__name__)
 	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://%s:%s@%s:%s/%s" % (
@@ -21,38 +25,11 @@ def main():
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 	db.init_app(app)
-
-	app.add_url_rule("/", "helloworld", hello_world, methods=["GET"])
-	app.add_url_rule("/<url_id>", "resolveurl", resolveURL, methods=["GET"])
-	app.add_url_rule("/app/create", "createurl", createURL, methods=["POST"])
+	app.register_blueprint(shortener_bp)
 
 	app.run(debug=True, host=config["default"]["host"], port=int(config["default"]["port"]))
 
-def hello_world():
-	return "<p>hello world</p>"
-
-def generateURLID(val: int, min_length:int=6, salt:str="testing"):
-	hid = Hashids(salt=salt, min_length=min_length)
-	return hid.encode(val)
-
-def createURL():
-	json_data = request.get_json(force=True)
-	original_url = json_data["original_url"]
-
-	shorturl_entry = ShortURL(original_url=original_url)
-
-	db.session.add(shorturl_entry)
-	db.session.flush()
-	shorturl_entry.url_id = generateURLID(shorturl_entry.id)
-	db.session.commit()
-
-	return jsonify({"status": "success", "short_url_id": shorturl_entry.url_id})
-
-def resolveURL(url_id):
-	shorturl_entry = ShortURL.query.filter_by(url_id=url_id).first()
-
-	return redirect(escape(shorturl_entry.original_url))
-
+	return app
 
 if __name__ == "__main__":
-	main()
+	init_app()
