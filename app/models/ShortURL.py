03be8f4911b6cf327f5bcf7d446dfb811a42ab61
chooshihao <chooshihao@gmail.com>
2022-09-09 18:30:03 +0800

    Revert "- updated requirements.txt"
    This reverts commit 6fa5a03058f3a67ff72d7985c24c7ab3cecb852d.

--- app/models/ShortURL.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class ShortURL(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	original_url = db.Column(db.String(2048), nullable=False)	
-	url_id = db.Column(db.String(10))
