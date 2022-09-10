cef2646d941ec2ae1f562961edeb89b433b555e4
chooshihao <chooshihao@gmail.com>
2022-09-10 10:28:00 +0800

    update app to switch between different configs

--- app/app.py
+++ app/app.py
@@ -4,30 +4,30 @@ from flask import Flask
 from models import db
 from shortener_bp import shortener_bp
 
-def loadConfig():
+def loadConfig(configFilename):
 	config = configparser.ConfigParser()
-	config.read(os.path.dirname(os.path.realpath(__file__)) + "/app.conf")
-	print(config.sections())
+	config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), configFilename))
 
 	return config
 
-def init_app():
-	config = loadConfig()
-
+def init_app(testing=False):
 	app = Flask(__name__)
-	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://%s:%s@%s:%s/%s" % (
-												config["database"]["username"],
-												config["database"]["password"],
-												config["database"]["host"],
-												config["database"]["port"],
-												config["database"]["database"],
-											)
+
+	if testing:
+		app.testing = True
+		config = loadConfig("test.conf")
+	else:
+		config = loadConfig("app.conf")
+
+	app.config["SQLALCHEMY_DATABASE_URI"] = config["default"]["db_uri"]
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 	db.init_app(app)
 	app.register_blueprint(shortener_bp)
 
-	app.run(debug=True, host=config["default"]["host"], port=int(config["default"]["port"]))
+	app.run(debug=bool(config["default"]["debug"])
+			, host=config["default"]["host"]
+			, port=int(config["default"]["port"]))
 
 	return app
 
