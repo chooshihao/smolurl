4118cc8d962d579b3c7e7c264f9044c5654cb75d
chooshihao <chooshihao@gmail.com>
2022-09-12 00:36:08 +0800

    done with basic funalities for frontned

---
+++ app/static/requests.js
@@ -0,0 +1,48 @@
+async function postShortenURL(event) {
+	const url = "/app/create";
+	const body = {
+		"original_url": document.getElementById("original_url").value
+	};
+
+	const response = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(body)
+	}).then(
+		function(res) {
+			if(res.status === 200) {
+				res.json().then(function(data){
+					onSuccessShorten(body.original_url, data.short_url_id)
+				});
+			}
+		}
+	).catch(function(error) {
+		console.log(error);
+	});
+}
+
+function setURL(url, target_element) {
+	target_element.value = url;
+}
+
+function onSuccessShorten(originalurl, shorturl) {
+	console.log(originalurl, shorturl);
+	setURL(originalurl, document.getElementById("long_url"));
+	setURL(shorturl, document.getElementById("short_url"));
+
+	document.getElementById("output").style.display = "";
+}
+
+function copyURLToClipboard(inputelement, event) {
+	inputelement.select();
+	inputelement.setSelectionRange(0, 99999);
+
+	navigator.clipboard.writeText(inputelement.value);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.getElementById("long_copy_btn").addEventListener("click", (event) => copyURLToClipboard(document.getElementById("long_url"), event));
+	document.getElementById("short_copy_btn").addEventListener("click", (event) => copyURLToClipboard(document.getElementById("short_url"), event));
+});
