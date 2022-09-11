ea503e5b7fd598f2773414cab8f6527aa24a4e18
chooshihao <chooshihao@gmail.com>
2022-09-11 17:42:55 +0800

    added some frontend components shortener_bp - added / endpoint - added some checks for missing  params when accessing /
    templates
    - added home page and 404 page

--- app/shortener_bp.py
+++ app/shortener_bp.py
@@ -1,16 +1,27 @@
-from flask import Blueprint, redirect, request, jsonify
+from urllib import response
+from flask import Blueprint, redirect, request, jsonify, render_template
 from markupsafe import escape
 from .models import ShortURL, db
 from hashids import Hashids
 
 shortener_bp = Blueprint("shortener_bp", __name__)
 
+@shortener_bp.route("/", methods=["GET"], strict_slashes=True)
+def renderHome():
+	return render_template("index.html")
+
 @shortener_bp.route("/<url_id>", methods=["GET"])
 def resolveURL(url_id):
+	if not url_id:
+		return
+
 	shorturl_entry = ShortURL.query.filter_by(url_id=url_id).first()
 	# TODO: handle invalid id
 
-	return redirect(escape(shorturl_entry.original_url))
+	if shorturl_entry:
+		return redirect(escape(shorturl_entry.original_url))
+	else:
+		return render_template("404.html"), 404
 
 def generateURLID(val: int, min_length:int=6, salt:str="testing"):
 	hid = Hashids(salt=salt, min_length=min_length)
